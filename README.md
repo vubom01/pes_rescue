fd77d2ba5bd2ff62c1732174c2d56fe93f278e61
huyvu <vu.lh@teko.vn>
2021-07-06 13:53:09 +0700

    README

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 $ git clone https://github.com/vubom01/vvv.git
 $ cd vvv
 $ virtualenv -p python3 .venv
-$ source .venv/bin/activate
+$ - source .venv/bin/activate (Ubuntu)
+  - .venv\Scripts\activate (Windows)
 $ pip install -r requirements.txt
 $ cd app
 $ python main.py
--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def hello():
     return 'Hello'
+
+@app.get('/index')
+def index():
+    user = {'username': 'Thai'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Myblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+
 
 if __name__ == '__main__':
     app.run()
