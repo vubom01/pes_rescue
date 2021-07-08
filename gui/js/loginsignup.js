ebc372355480fac2bd7a76b3fa01d6a4646c3dd4
Vinh Kieu <transformers9510@gmail.com>
2021-07-08 21:33:45 +0700

    test gui login

---
+++ gui/js/loginsignup.js
@@ -0,0 +1,5 @@
+const userAction = async () => {
+  const response = await fetch('http://example.com/movies.json');
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+}
