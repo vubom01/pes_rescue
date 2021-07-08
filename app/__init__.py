0c67f3e8880004cee4d0430633c15f71a5572606
huyvu <vu.lh@teko.vn>
2021-07-08 21:57:18 +0700

    fix database name

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,6 @@ app.secret_key = '123456'
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'pes_rescue'
+app.config['MYSQL_DB'] = 'pet_rescue'
 
 mysql = MySQL(app)
