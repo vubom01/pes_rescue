6446cda3634d571413849a703baa7f8ea010ec47
huyvu <vu.lh@teko.vn>
2021-07-10 17:42:24 +0700

    fix login

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.api_router import router
 
-app = FastAPI()
+def get_application() -> FastAPI:
+    application = FastAPI()
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in ['*']],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    # application.add_middleware(DBSessionMiddleware, db_url=settings.DATABASE_URL)
+    application.include_router(router, prefix='')
+    # application.add_exception_handler(CustomException, http_exception_handler)
 
-app.include_router(router=router, prefix='')
+    return application
+
+
+app = get_application()
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
--- gui/js/loginsignup.js
+++ gui/js/loginsignup.js
@@ -1,26 +1,23 @@
-
 async function getUser(event) {
     event.preventDefault();
     // Default options are marked with *
     let url = 'http://127.0.0.1:8000/login';
-    let data = `{
-        "username": "test", 
-        "password": "test"
-    }`
-    
+    let data = {
+        username: "test",
+        password: "test"
+    }
+
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'no-cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      header: { 'Content-Type' : 'application/json' },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.parse(data) // body data type must match "Content-Type" header
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     });
-    console.log(response) ; // parses JSON response into native JavaScript objects
-  }
-  
+    console.log(response);
+}
+
 
 let button = document.getElementById('login-button')
 button.addEventListener('click', getUser);
