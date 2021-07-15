a46dbf0699a7bebd87ea681fcd5ca5d203588e82
huyvu <vu.lh@teko.vn>
2021-07-15 12:58:49 +0700

    hash password and order import

--- app/core/security.py
+++ app/core/security.py
@@ -1,9 +1,10 @@
-import jwt
-
-from typing import Any, Union
-from app.core.config import settings
 from datetime import datetime, timedelta
+from typing import Any, Union
+
+import jwt
 from passlib.context import CryptContext
+
+from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -17,3 +18,9 @@ def create_access_token(user_id: Union[int, Any]) -> str:
     }
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.SECURITY_ALGORITHM)
     return encoded_jwt
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
