f3ec986b3bbe7c2cfdf1626d6e00e101c1136841
huyvu <vu.lh@teko.vn>
2021-07-15 11:20:44 +0700

    fix register

--- app/api/api_register.py
+++ app/api/api_register.py
@@ -5,10 +5,10 @@ from app.services.srv_user import UserService
 router = APIRouter()
 
 @router.post('')
-def login(request: UserRegisterRequest):
+def register(request: UserRegisterRequest):
     exist_user = UserService.is_exist_user(username=request.username)
     if exist_user:
-        raise HTTPException(status_code=400, detail='Incorrect email or password')
+        raise HTTPException(status_code=400, detail='Username is already exist')
     UserService.register_user(data=request)
     user_id = UserService.is_exist_user(username=request.username)['id']
     return {
