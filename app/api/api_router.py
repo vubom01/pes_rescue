1a405cc65d55f5e99cc4891be95271c1f6ed8708
son-of-win <hackeranonymous1310@gmail.com>
2021-07-26 20:38:11 +0700

    api update role of user

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from app.api import api_login, api_register, api_user, api_pet
+from app.api import api_login, api_register, api_user
 
 router = APIRouter()
 
 router.include_router(api_login.router, tags=["login"], prefix="/login")
 router.include_router(api_register.router, tags=["register"], prefix="/register")
 router.include_router(api_user.router, tags=["user"], prefix="/user")
-router.include_router(api_pet.router, tags=["pet"], prefix="/pet")
+
 
