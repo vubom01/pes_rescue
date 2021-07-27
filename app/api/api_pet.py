773e73ad7c88693b1fd67646b50fd597739abcd6
son-of-win <hackeranonymous1310@gmail.com>
2021-07-27 16:52:11 +0700

    fix upload image

--- app/api/api_pet.py
+++ app/api/api_pet.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from starlette import status
 
 from app.helpers.login_manager import PermissionRequired, login_required
+from app.schemas.sche_pet import ListPets
 from app.services.srv_pet import PetService
 
 logger = logging.getLogger()
@@ -11,6 +12,7 @@ router = APIRouter()
 
 
 @router.post('/images', dependencies=[Depends(PermissionRequired('admin'))])
-def upload_pet_image(pet_id: int, image_base64: str):
+def upload_pet_image(pet_id: int, file: UploadFile = File(...)):
+    return PetService.upload_pet_image(pet_id=pet_id, image=file.file)
 
-    return PetService.upload_pet_image(pet_id=pet_id, image_base64=image_base64)
+
