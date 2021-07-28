65b9c051c31539f6e60534c990d96e8ad8323b29
son-of-win <hackeranonymous1310@gmail.com>
2021-07-28 07:41:56 +0700

    get pet by id

---
+++ app/schemas/sche_pet.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class PetItemResponse(BaseModel):
+    name: str
+    age: str
+    color: str
+    health_condition: str
+    weight: float
+    species: str
+    url: str
+    description: Optional[str]
+
+
+class ListPets(BaseModel):
+    users: List[PetItemResponse]
