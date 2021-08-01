06c863c1aa3fd67507189fe292288e8f56c23932
huyvu <vu.lh@teko.vn>
2021-08-01 11:18:26 +0700

    config db

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,10 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    MYSQL_HOST = 'us-cdbr-east-04.cleardb.com'
-    MYSQL_USER = 'bb77b7bc8d9d31'
-    MYSQL_PASSWORD = '23e164c8'
-    MYSQL_DB = 'heroku_b6f0bc9b22feae3'
+    MYSQL_HOST = 'sql6.freemysqlhosting.net'
+    MYSQL_USER = 'sql6428642'
+    MYSQL_PASSWORD = 'SAwqck96ft'
+    MYSQL_DB = 'sql6428642'
     API_PREFIX = ''
     BACKEND_CORS_ORIGINS = ['*']
     ACCESS_TOKEN_EXPIRE_SECONDS: int = 60 * 60 * 24 * 7  # Token expired after 7 days
