f1fb71300e34c8ee9915b76f1a59154a328f2b6a
son-of-win <hackeranonymous1310@gmail.com>
2021-08-03 15:10:30 +0700

    api sponsor service

---
+++ app/services/srv_sponsor.py
@@ -0,0 +1,52 @@
+from app.db.base import mysql
+from app.schemas.sche_sponsor import SponsorRequest
+
+
+class SponsorService(object):
+    @staticmethod
+    def is_exist_sponsor(phone_number: str):
+        cursor = mysql.cursor()
+        query = 'select * from sponsors where phone_number = %s'
+        cursor.execute(query, (phone_number,))
+        sponsor = cursor.fetchone()
+        if not sponsor:
+            return None
+        return sponsor
+
+    @staticmethod
+    def create_sponsor(data: SponsorRequest):
+        cursor = mysql.cursor()
+        query = 'insert into sponsors (first_name, last_name,address, phone_number, email) values (%s, %s, %s, %s, %s)'
+        cursor.execute(query, (data.first_name, data.last_name, data.address, data.phone_number, data.email))
+        mysql.commit()
+
+    @staticmethod
+    def get_list_sponsors():
+        cursor = mysql.cursor()
+        query = 'select * from sponsors;'
+        cursor.execute(query)
+        sponsors = cursor.fetchall()
+        return sponsors
+
+    @staticmethod
+    def get_sponsor_detail(id: int):
+        cursor = mysql.cursor()
+        query = 'select * from sponsors where id = %s;'
+        cursor.execute(query, (id,))
+        sponsor = cursor.fetchone()
+        return sponsor
+
+    @staticmethod
+    def update_info_sponsor(id: int, data: SponsorRequest):
+        cursor = mysql.cursor()
+        query = 'update sponsors set first_name = %s, last_name = %s, address = %s, phone_number = %s, email = %s \
+            where id = %s'
+        cursor.execute(query, (data.first_name, data.last_name, data.address, data.phone_number, data.email,id,))
+        mysql.commit()
+
+    @staticmethod
+    def delete_sponsor(id: int):
+        cursor = mysql.cursor()
+        query = 'delete from sponsors where id = %s'
+        cursor.execute(query, id)
+        mysql.commit()
