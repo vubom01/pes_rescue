8ff5bb30ddd1e1bffa3dc490b75886ba49b6ca4f
huyvu <vu.lh@teko.vn>
2021-08-04 08:15:55 +0700

    add log for not connect mail config

--- app/api/api_mail.py
+++ app/api/api_mail.py
@@ -1,7 +1,7 @@
 import logging
 from typing import List, Optional
 
-from fastapi import APIRouter, BackgroundTasks, Depends
+from fastapi import APIRouter, Depends
 from fastapi_mail import FastMail, MessageSchema
 from pydantic import BaseModel, EmailStr
 
@@ -16,14 +16,14 @@ class BodyEmail(BaseModel):
     body: str
 
 @router.post('', dependencies=[Depends(PermissionRequired('admin', 'volunteer'))])
-async def send_mail(background_tasks: BackgroundTasks, email: List[EmailStr], body_mail: BodyEmail):
+async def send_mail(email: List[EmailStr], body_mail: BodyEmail):
     message = MessageSchema(
         subject=body_mail.subject,
         recipients=email,
         body=body_mail.body
     )
     fm = FastMail(mail_config)
-    background_tasks.add_task(fm.send_message, message)
+    await fm.send_message(message)
     return {
         'message': 'email has been sent'
     }
