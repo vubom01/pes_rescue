41ab81903b3388d2b15d4e9646c2b9cc899b4df4
huyvu <vu.lh@teko.vn>
2021-08-04 07:47:25 +0700

    config

--- app/api/api_mail.py
+++ app/api/api_mail.py
@@ -1,6 +1,9 @@
+from typing import List, Optional
+
 from fastapi import BackgroundTasks
 from fastapi_mail import FastMail, MessageSchema
-from app.schemas.sche_mail import EmailSchema, BodyEmail
+from pydantic import EmailStr, BaseModel
+
 from app.db.base import mail_config
 from fastapi import APIRouter, Depends
 from app.helpers.login_manager import PermissionRequired
@@ -9,12 +12,15 @@ import logging
 logger = logging.getLogger()
 router = APIRouter()
 
+class BodyEmail(BaseModel):
+    subject: Optional[str]
+    body: str
 
 @router.post('', dependencies=[Depends(PermissionRequired('admin', 'volunteer'))])
-async def send_mail(background_tasks: BackgroundTasks, email: EmailSchema, body_mail: BodyEmail):
+async def send_mail(background_tasks: BackgroundTasks, email: List[EmailStr], body_mail: BodyEmail):
     message = MessageSchema(
         subject=body_mail.subject,
-        recipients=email.dict().get("email"),
+        recipients=email,
         body=body_mail.body
     )
     fm = FastMail(mail_config)
